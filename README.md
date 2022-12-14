c2579aeb038baecb89a35d0d327d7cff698495ff
tajultonim <mail@tajultonim.com>
2022-12-14 12:58:28 +0600

    ok

---
+++ README.md
@@ -0,0 +1,9 @@
+# EXPRESS EJS TEST
+
+#### This repo is to test Express app deployed on Render. It simply returns the path you are sending GET or POST request to.
+
+### It uses:
+
+- express
+- ejs
+- nodemon (dev)
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use("/styles", express.static("styles"));
 app.set("view engine", "ejs");
 
 app.get("*", (req, res) => {
-  console.log("GET /");
+  console.log("GET " + req.path);
   res.render("pages/index", { path: req.path });
 });
 
-app.post("/", (req, res) => {});
+app.post("*", (req, res) => {
+  console.log("POST " + req.path);
+  res.send("POST " + req.path);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("listening");
